3407b0d8714b06084af37013d3cc72d7ae57a541
Geoffrey J. Teale <geoffrey@avocet.io>
2017-08-03 09:36:48 +0200

    Test the handler stored the correct publish function.

--- nsqhandler_test.go
+++ nsqhandler_test.go
@@ -5,12 +5,20 @@ import (
 )
 
 func TestNew(t *testing.T) {
+	called := false
 	fakePublish := func(topic string, body []byte) error {
+		called = true
 		return nil
 	}
 	handler := New(fakePublish)
 	if handler == nil {
 		t.Fatal("Expected *Handler, got nil")
 	}
-
+	if handler.pfunc == nil {
+		t.Fatal("Expected pfunc to be set, but it was not")
+	}
+	handler.pfunc("foo", nil)
+	if !called {
+		t.Fatal("Expect fakePublish to be called, but it was not.")
+	}
 }
