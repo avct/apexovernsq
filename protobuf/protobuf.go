d5b2cb233fb6793be387f161ef0bfa794cc3ccb1
Geoffrey J. Teale <geoffrey@avocet.io>
2017-08-09 11:14:32 +0200

    Test marshalling/unmarshalling errors set with WithError()

--- protobuf/protobuf.go
+++ protobuf/protobuf.go
@@ -10,11 +10,17 @@ import (
 // Marshal is an implementation of a MarshalFunc specifically for use
 // with this handler.  Although it accepts an empty interface type, it
 // will only work with an apex.log.Entry type, and will panic if any
-// other type is passed in.
+// other type is passed in.  Not that this mechanism also enforces the
+// rule that any fields set must either be strings or satisfy the
+// fmt.Stringer interface.
 func Marshal(x interface{}) ([]byte, error) {
 	var logEntry *alog.Entry
 	var timestamp []byte
 	var ok bool
+	var fields map[string]string
+	var stringer fmt.Stringer
+	var str string
+
 	if logEntry, ok = x.(*alog.Entry); !ok {
 		return nil, fmt.Errorf("Attempted to marshal a type other than apex.log.Entry")
 	}
@@ -22,14 +28,30 @@ func Marshal(x interface{}) ([]byte, error) {
 	if err != nil {
 		return nil, err
 	}
+
+	fields = make(map[string]string, len(logEntry.Fields))
+	for key, value := range logEntry.Fields {
+		if str, ok = value.(string); ok {
+			fields[key] = str
+			continue
+		}
+		if stringer, ok = value.(fmt.Stringer); !ok {
+			err := fmt.Errorf("Value for field %s is not a string, nor does it satisfy fmt.Stringer", key)
+			return nil, err
+		}
+		fields[key] = stringer.String()
+	}
 	entry := &Entry{
 		Level:     logEntry.Level.String(),
 		Timestamp: timestamp,
 		Message:   logEntry.Message,
+		Fields:    fields,
 	}
 	return proto.Marshal(entry)
 }
 
+// Unmarshal is an implementation of a UnmarshalFunc specifically for
+// unmarshalling an Entry back into apex.log.Entry.
 func Unmarshal(data []byte, v interface{}) error {
 	var entry *Entry
 	var logEntry *alog.Entry
@@ -46,5 +68,12 @@ func Unmarshal(data []byte, v interface{}) error {
 	}
 	logEntry.Level = alog.MustParseLevel(entry.Level)
 	logEntry.Timestamp.UnmarshalText(entry.Timestamp)
+	logEntry.Message = entry.Message
+	if logEntry.Fields == nil {
+		logEntry.Fields = make(map[string]interface{}, len(entry.Fields))
+	}
+	for key, value := range entry.Fields {
+		logEntry.Fields[key] = value
+	}
 	return nil
 }
